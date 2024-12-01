cbfc18a58d72daf344c4035ade71fc4975f5b69f
SajeevNairS <sajnairs@gmail.com>
2024-12-01 15:20:16 -0500

    Advanced 8 image augmentation

--- utils/augmentation_viz.py
+++ utils/augmentation_viz.py
@@ -8,10 +8,14 @@ def visualize_augmentations(image, num_samples=5, digit=None):
     try:
         digit_str = f"digit_{digit}" if digit is not None else "sample"
         
-        # Define simple augmentations
+        # Define augmentations
         augmentations = [
             ("Rotation", transforms.RandomRotation(degrees=5)),
-            ("Affine", transforms.RandomAffine(degrees=5, translate=(0.05, 0.05)))
+            ("Affine", transforms.RandomAffine(degrees=5, translate=(0.05, 0.05))),
+            ("Combined", transforms.Compose([
+                transforms.RandomRotation(degrees=5),
+                transforms.RandomAffine(degrees=5, translate=(0.05, 0.05))
+            ]))
         ]
         
         # Save original image
@@ -19,19 +23,23 @@ def visualize_augmentations(image, num_samples=5, digit=None):
         plt.imshow(image.squeeze(), cmap='gray')
         plt.title(f'Original Digit {digit}')
         plt.axis('off')
-        plt.savefig(f'visualizations/augmentations/original_{digit_str}.png')
+        plt.savefig(f'visualizations/augmentations/original_{digit_str}.png', 
+                   bbox_inches='tight', dpi=150, facecolor='white')
         plt.close()
         
         # Generate augmented samples
         for aug_name, aug_transform in augmentations:
-            plt.figure(figsize=(15, 3))
+            fig = plt.figure(figsize=(15, 3))
+            fig.suptitle(f'{aug_name} Augmentations', y=1.05)
             for i in range(num_samples):
                 plt.subplot(1, num_samples, i+1)
                 augmented = aug_transform(image)
                 plt.imshow(augmented.squeeze(), cmap='gray')
-                plt.title(f'{aug_name} {i+1}')
+                plt.title(f'Sample {i+1}')
                 plt.axis('off')
-            plt.savefig(f'visualizations/augmentations/{aug_name.lower()}_{digit_str}.png')
+            plt.tight_layout()
+            plt.savefig(f'visualizations/augmentations/{aug_name.lower()}_{digit_str}.png',
+                       bbox_inches='tight', dpi=150, facecolor='white')
             plt.close()
             
         return True
